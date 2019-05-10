1852544de6a1ee573fec58a5e13faafaf21681c4
Aman Gupta <aman@tmm1.net>
2019-05-09 18:39:35 -0700

    add hgtv

---
+++ channels/hgtv.js
@@ -0,0 +1,59 @@
+var ChannelClass = require('../lib/ChannelClass.js');
+const Provider = require('../lib/Provider.js');
+const provider = new Provider;
+
+class Channel extends ChannelClass {
+
+  constructor(args) {
+    super(args);
+    this.GuideName = "HGTV";
+    this.GuideNumber = 1310;
+    this.HD = true;
+    this.ScanInterval = 120;
+  }
+
+  InitializeChannel() {
+
+        const _self = this;
+        if(this.HasPlaylist(this.ScanInterval)) {
+            console.log("Already have playlist URL for channel %s", this.GuideName);
+            this.Ready();
+            return true;
+        }
+
+        if(this.Scanning && this.SkipScan) {
+            console.log("Scanning is disabled for %s", this.GuideName);
+            this.Ready();
+            return true;
+        }
+
+        (async() => {
+            provider.Provider = this.Provider;
+            provider.Username = this.Username;
+            provider.Password = this.Password;
+            provider.RequestorId = "HGTV";
+            provider.RedirectUrl = "https://watch.hgtv.com/watch/hgtv"
+            provider.RequestMatch = "channel/.+\\.m3u8"
+            provider.Debug = true;
+            provider.DataDir = './newdatadir';
+            provider.on('Ready', function(url) {
+                console.log("Got URL %s", url);
+                _self.PlaylistURL = url;
+                _self.Available = true;
+                _self.Ready();
+                provider.Close();
+            });
+            provider.on('Error', function(error) {
+                console.log("Error from provider %s", error);
+                _self.Error(error);
+                _self.Available = false;
+                provider.Close();
+            });
+            provider.Navigate().then(function() {
+                console.log("Navigation complete");
+            });
+        })();
+    }
+}
+
+module.exports = Channel;
